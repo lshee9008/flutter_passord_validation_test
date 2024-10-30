ac961ee48a80ff41038382739e006e029e7e16f4
lshee9008 <9008lucky@naver.com>
2024-10-31 00:35:02 +0900

    d

--- lib/screens/PasswordFormValidator.dart
+++ lib/screens/PasswordFormValidator.dart
@@ -9,7 +9,9 @@ class Passwordformvalidator extends StatefulWidget {
 }
 
 class _PasswordformvalidatorState extends State<Passwordformvalidator> {
+  final TextEditingController controller = TextEditingController();
   bool isSuccess = false; // initially
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -29,37 +31,64 @@ class _PasswordformvalidatorState extends State<Passwordformvalidator> {
         padding: EdgeInsets.all(
           15,
         ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 200,
-              width: double.infinity,
-              child: isSuccess
-                  ? Lottie.network(
-                      "https://lottie.host/c288e345-7b57-45c8-b224-cc6af457bfe0/7wi1v8ZrWM.json")
-                  : Lottie.network(
-                      "https://lottie.host/de8895c3-39b1-488d-8089-d42c697bd7f2/8q4PKhMEyb.json"),
-            ),
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: isSuccess
+                    ? Lottie.network(
+                        "https://lottie.host/c288e345-7b57-45c8-b224-cc6af457bfe0/7wi1v8ZrWM.json")
+                    : Lottie.network(
+                        "https://lottie.host/de8895c3-39b1-488d-8089-d42c697bd7f2/8q4PKhMEyb.json"),
+              ),
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    TextField(
+                      obscureText: _obscureText,
+                      controller: controller,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                        borderRadius: BorderRadius.circular(20),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: isSuccess ? Colors.green : Colors.red,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: "Password",
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          icon: Icon(
+                            _obscureText
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 15,
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
